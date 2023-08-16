89ede71ea4964baddeee0bba4d06d6b5bf15f453
axem4n <toporov.den2@list.ru>
2023-08-16 21:01:21 +0300

    autorization-frontEnd

--- fullStack/frontEnd/src/components/autorization/Autorization.jsx
+++ fullStack/frontEnd/src/components/autorization/Autorization.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import DraconImg from '../../image/drakonEgor.png'
+import logo from "../../image/logo.png"
 import './autorization.css'
 
 export class Autorization extends React.Component{
@@ -8,15 +9,25 @@ export class Autorization extends React.Component{
         return(
             <>
                 <div className="bg-autorization">
-                    <div className="content-autorization">
-                        <b>ВОЙТИ</b>
-                        <form>
-                            <div className="form-input-autorization">
-                                <input placeholder="Введите email" type="email" name="email" />
-                                <input placeholder="Введите пароль" type="password" name="password" />
-                            </div>
-                            <button type="submit" formMethod="post">ВОЙТИ</button>
-                        </form>
+                    <img src={DraconImg} width="700px" alt=""  className="dragon-img-autorization"/>
+                    <div className="bg-content-autorization">
+                        <div className="content-autorization">
+                            <b>ВОЙТИ</b>
+                            <form>
+                                <div className="form-input-autorization">
+                                    <input placeholder="Введите email" type="email" name="email" />
+                                    <input placeholder="Введите пароль" type="password" name="password" />
+                                </div>
+                                <button type="submit" formMethod="post">ВОЙТИ</button>
+                            </form>
+
+                            <a href=""><span>ЗАБЫЛ ПАРОЛЬ</span></a><br />
+                            <a href=""> ЗАРЕГИСТРИРОВАТЬСЯ</a>
+                            
+                            <img src={logo} alt="" width="110px" className="logo-content-autorization" />
+                        </div>
+                        
+                        
                     </div>
                 </div>
             </>
