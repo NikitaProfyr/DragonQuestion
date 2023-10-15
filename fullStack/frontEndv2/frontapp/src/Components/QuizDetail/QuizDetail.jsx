aaae98743a2328c4fe85f305cffd85b5e4a49ac9
Nikita Profir <neicker44536@gmail.com>
2023-10-15 16:32:16 +0300

    15.10.2023 add quiz detail title description count questions in page

--- fullStack/frontEndv2/frontapp/src/Components/QuizDetail/QuizDetail.jsx
+++ fullStack/frontEndv2/frontapp/src/Components/QuizDetail/QuizDetail.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import {Container} from 'react-bootstrap'
+
+import './quiz-detail.css'
 
 const QuizDetail = () => {
   const quiz = useSelector(state => state.reducerQuiz.currentQuiz)
   console.log(quiz);
   return (
-    <div className="wrapper">
-      test
+    <div className="bg-quiz-detail">
+      <div className="wrapper">
+        <Container fluid className='d-flex justify-content-beetwen'>
+          <div className="left-content">
+            <h3>{quiz.title}</h3>
+            <h4>{quiz.description}</h4>
+            <h4>Количество вопросов: {quiz.question.length}</h4>
+          </div>
+          <div className="right-content">
+
+          </div>
+        </Container>
+      </div>
     </div>
   )
 }
